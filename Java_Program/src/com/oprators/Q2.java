d3c1501a7c773e3d9da4bdbf67af4f89661973c5
prashantkalokhe <82570721+prashantkalokhe@users.noreply.github.com>
2023-10-31 12:51:26 +0530

    first commit

---
+++ Java_Program/src/com/oprators/Q2.java
@@ -0,0 +1,28 @@
+package com.oprators;
+
+public class Q2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/*
+		 * Q1 int a=3,b=-3; int z= ++b - --a- ++b -a++ + b++ -a++;
+		 * System.out.println("value of a is"+ a + "value of b is :"+ b
+		 * +"value of z is : "+ z); //a=4,b=0,z=-9
+		 */
+
+		/*
+		 * Q2 int x=5,y=2,z=2; int a= x++ - --x - --y +z +z++ - --z - --x +z+y ;
+		 * System.out.println("value of x is"+ x + "value of y is :"+ y
+		 * +"value of z is : "+ z+"value of a is : "+ a); //x=4,y=1,z=2,a=0
+		 */
+
+		/*
+		 * Q3 int x=5,y=2,z=2; int a= --z - --y +z++ + ++x - --x - --x -z-- +y++ ;
+		 * System.out.println("value of x is"+ x + "value of y is :"+ y
+		 * +"value of z is : "+ z+"value of a is : "+ a); //x=4,y=2,z=1,a=-3
+		 */
+
+	}
+
+}
